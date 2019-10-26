c404d7814b71a85340b0aafde8d982334316f7a9
seeris <sergei.udris@gmail.com>
2019-10-26 07:27:30 +0300

    math

--- src/data/pad/data/core.clj
+++ src/data/pad/data/core.clj
@@ -1,13 +1,13 @@
 (ns pad.data.core)
 
 
-(defn str->float
+(defn str>>float
   ([s]
-   (str->float s s))
+   (str>>float s s))
   ([s alt]
    (try (Float/parseFloat s)
         (catch Exception e alt))))
 
 (defn str-float?
   [s]
-  (number? (str->float s)))
+  (number? (str>>float s)))
