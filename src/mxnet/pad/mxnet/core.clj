7eb430ccd2b064298116acf416bcfacd3e15ad2e
seeris <sergei.udris@gmail.com>
2019-11-15 15:53:16 +0300

    mxnet

--- src/mxnet/pad/mxnet/core.clj
+++ src/mxnet/pad/mxnet/core.clj
@@ -14,6 +14,7 @@
             [org.apache.clojure-mxnet.initializer :as initializer]
             [org.apache.clojure-mxnet.resource-scope :as resource-scope]
             [org.apache.clojure-mxnet.ndarray :as nd]
+            [org.apache.clojure-mxnet.ndarray-api :as ndapi]
             [org.apache.clojure-mxnet.dtype :as dtype]
             [org.apache.clojure-mxnet.callback :as callback]
             [org.apache.clojure-mxnet.layout :as layout]
@@ -37,8 +38,11 @@
   (let [xs (->> (io/reader path)
                 (line-seq)
                 (lines>>word-embeddings))]
-    {:idx-to-token (mapv first xs)
-     :token-to-embedding (into {} xs)}))
+    {:vec (vec xs)
+     :idx-to-token (mapv first xs)
+     :token-to-embedding (into {} xs)
+     :token-to-idx (->> xs (map-indexed #(vector (first %2) %1)) (into {}))
+     }))
 
 (defn glove-path
   [glove-dir embedding-size]
@@ -48,6 +52,8 @@
   [a b]
   (nd// (nd/dot a b) (nd/* (nd/norm a) (nd/norm b))))
 
+
+
 #_(def a (nd/array [1 2] [2]))
 #_(def b (nd/array [-1 -2] [2]))
 #_(def c (nd/array [10 20] [2]))
@@ -55,4 +61,36 @@
 #_(nd/->vec (cos-sim a b))
 #_(nd/->vec (cos-sim a c))
 
+(defn normalize
+  [a]
+  (ndapi/broadcast-div a (nd/norm a)))
 
+(defn normalize-row
+  [v]
+  (let [a (nd/array v [(count v)])]
+    (as-> a x
+      (nd/* x x)
+      (nd/sum x)
+      (nd/+ x 1E-10)
+      (nd/sqrt x)
+      (nd/broadcast-div a x))))
+
+#_(nd/norm (nd/array [1 2 3 4] [4]))
+#_(normalize (nd/array [1 2 3 4] [4]))
+#_(nd/norm (normalize (nd/array [1 2 3 4] [4])))
+#_(def a (nd/array [1 2 3 4] [4]))
+#_(def b (normalize a))
+
+(defn build-vocab
+  "Returns {word idx}"
+  [tokens]
+  (let [words (flatten tokens)
+        freq (reduce (fn [a word]
+                       (update-in a [word] (fnil inc 0))) {} words)
+        freq-sorted (sort-by second > freq)
+        words-sorted (map first freq-sorted)]
+    {:frequencies freq
+     :frequencies-sorted freq-sorted
+     :indexes (->>
+               (map vector words-sorted (range 0 (count words-sorted)))
+               (into {}))}))
