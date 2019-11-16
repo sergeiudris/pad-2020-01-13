5de304e5307333630d3aa33936d2c9fee06b9f78
seeris <sergei.udris@gmail.com>
2019-11-16 08:40:23 +0300

    (build) jvm memory

--- src/core/pad/core.clj
+++ src/core/pad/core.clj
@@ -3,6 +3,26 @@
 
 (def resolve-var pad.comm/resolve-var)
 
+(defn java-version
+  []
+  (System/getProperty "java.vm.version"))
+
+(comment
+
+  (System/getProperty "java.vm.version")
+  (System/getProperty "java.version")
+  (System/getProperty "java.specification.version")
+  (clojure-version)
+
+  (.. Runtime getRuntime freeMemory)
+
+  (-> (Runtime/getRuntime) (.maxMemory) (/ 1000000) (int)  (str "mb"))
+  (-> (Runtime/getRuntime) (.freeMemory) (/ 1000000) (int)  (str "mb"))
+  (-> (Runtime/getRuntime) (.totalMemory) (/ 1000000) (int)  (str "mb"))
+
+  ;
+  )
+
 (defn str>>float
   ([s]
    (str>>float s s))
--- src/prn/pad/prn/core.clj
+++ src/prn/pad/prn/core.clj
@@ -8,20 +8,7 @@
             ;
             ))
 
-(defn java-version
-  []
-  (System/getProperty "java.vm.version"))
 
-(comment
-
-
-  (System/getProperty "java.vm.version")
-  (System/getProperty "java.version")
-  (System/getProperty "java.specification.version")
-  (clojure-version)
-
-  ;
-  )
 
 (defn linst-methods
   [v]
