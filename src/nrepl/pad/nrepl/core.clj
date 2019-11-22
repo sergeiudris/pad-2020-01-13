da1c0f544160c3ac6e9a2c42c589eaa554813bb1
seeris <sergei.udris@gmail.com>
2019-11-22 10:08:30 +0300

    :nrepl require cider-nrepl-handler statically

--- src/nrepl/pad/nrepl/core.clj
+++ src/nrepl/pad/nrepl/core.clj
@@ -1,18 +1,19 @@
 (ns pad.nrepl.core
   (:require [nrepl.server :refer [start-server stop-server]]
             [clojure.repl :refer :all]
+            [cider.nrepl :refer [cider-nrepl-handler]]
    ;
             ))
 
-(defn nrepl-handler []
+#_(defn nrepl-handler []
   (require 'cider.nrepl)
   (ns-resolve 'cider.nrepl 'cider-nrepl-handler))
 
-(defn start-nrepl-server [& {:keys [host port] :or {port 7888 host "0.0.0.0"} }]
+(defn start-nrepl-server [& {:keys [host port] :or {port 7888 host "0.0.0.0"}}]
   (prn "--started nREPL server on 7888 ")
   (start-server
    :bind host
    :port port
-   :handler (nrepl-handler)
+   :handler cider-nrepl-handler #_(nrepl-handler)
    :middleware '[]))
 
